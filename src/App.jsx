df5ad5a0fbc942cfe4b8357bc5597ec2e3a4b5b3
ai-for-all-coders <ask.aiforall@gmail.com>
2025-12-31 13:19:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
 import AboutUs from "./components/AboutUs";
 import ProductList from "./components/ProductList";
 import CartPage from "./components/CartPage";
 
 export default function App() {
-  const totalQuantity = useSelector(state => state.cart.totalQuantity);
-
   return (
     <BrowserRouter>
-      <header className="navbar">
-        <h2>Paradise Nursery</h2>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/products">Products</Link>
-          <Link to="/cart">Cart ({totalQuantity})</Link>
-        </nav>
-      </header>
-
       <Routes>
-        <Route path="/" element={<AboutUs />} />
+        <Route
+          path="/"
+          element={
+            <div className="landing">
+              <h1>Welcome to Paradise Nursery</h1>
+              <Link to="/products">
+                <button>Get Started</button>
+              </Link>
+            </div>
+          }
+        />
         <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
